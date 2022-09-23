c3ec294701de7799f6c1d200f7fbecac5df32e29
Admin <kolt.offwork@gmail.com>
2022-09-23 14:06:42 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace ToDoListWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var culterInfo = new CultureInfo("ru_RU");
+            Thread.CurrentThread.CurrentCulture = culterInfo;
+            Thread.CurrentThread.CurrentUICulture = culterInfo;
+            CultureInfo.DefaultThreadCurrentCulture = culterInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = culterInfo;
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Services/FileIOService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListWPF.Models;
+
+namespace ToDoListWPF.Services
+{
+    public class FileIOService
+    {
+        private readonly string Path;
+
+        public FileIOService(string path)
+        {
+            Path = path;
+        }
+
+        public BindingList<ToDoModel> LoadData()
+        {
+            var fileExists = File.Exists(Path);
+            if (!fileExists)
+            {
+                File.CreateText(Path).Dispose();
+
+                return new BindingList<ToDoModel>()
+                {
+                    new ToDoModel()
+                    {
+                        IsDone = false,
+                        CreateDate = DateTime.Now,
+                        Text = "Empty",
+                    }
+                };
+            }
+            using (var reader = File.OpenText(Path))
+            { 
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object toDoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(Path))
+            {
+                string output = JsonConvert.SerializeObject(toDoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
